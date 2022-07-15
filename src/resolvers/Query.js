0fa27b90436d1b670bde61b92dd6e47133b7732b
SaidAoussar <aoussarsaid33@gmail.com>
2022-07-15 22:52:16 +0100

    add more feature to feed like filter and order

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,28 @@
 const feed = (parent, args, context) => {
-  const allLinks = context.prisma.link.findMany();
-  return allLinks;
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } }
+        ]
+      }
+    : {};
+
+  const links = context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy
+  });
+
+  const count = context.prisma.link.count({
+    where
+  });
+
+  return {
+    links,
+    count
+  };
 };
 
 const link = (parent, args, context) => {
